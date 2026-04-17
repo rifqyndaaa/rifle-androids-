d7742f209cf032226dd2e56d3e5373331eb67065
rifqyndaaa <yanda24si@mahasiswa.pcr.ac.id>
2026-04-17 09:50:24 +0700

    p4 part2

--- app/src/main/java/com/example/myapplication/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/myapplication/pertemuan_4/FourthActivity.kt
@@ -1,43 +1,74 @@
 package com.example.myapplication.pertemuan_4
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.myapplication.MainActivity
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityFourthBinding
-import com.example.myapplication.databinding.ActivityThirdBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 
 class FourthActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityFourthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         binding = ActivityFourthBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
             insets
         }
 
+        // Ambil Data dari Intent
         val name = intent.getStringExtra("name")
         val from = intent.getStringExtra("from")
-        val age = intent.getIntExtra("age",0)
-        Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
+        val age = intent.getIntExtra("age", 0)
 
+        Log.e("Data Intent", "Nama: $name , Usia: $age, Asal: $from")
 
+        // Tombol Submit
         binding.btnSubmit2.setOnClickListener {
             finish()
+        }
 
+        // Tombol Snackbar
+        binding.btnShowSnackbar.setOnClickListener {
+            Snackbar.make(binding.root, "Ini adalah Snackbar", Snackbar.LENGTH_SHORT)
+                .setAction("Tutup") {
+                    finish()
+                    Log.e("Info Snackbar", "Snackbar ditutup")
+                }
+                .show()
+        }
 
-
+        // Tombol Alert Dialog
+        binding.btnShowAlertDialog.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Konfirmasi")
+                .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog", "Anda memilih Ya!")
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog", "Anda memilih Tidak!")
+                }
+                .show()
         }
 
         Log.e("onCreate", "FourthActivity dibuat pertama kali")
@@ -45,12 +76,11 @@ class FourthActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        Log.e("onStart", "onStart: fourthActivity terlihat di layar")
+        Log.e("onStart", "FourthActivity terlihat di layar")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.e("onDestroy", "fourthActivity dihapus dari stack")
+        Log.e("onDestroy", "FourthActivity dihapus dari stack")
     }
-
 }
