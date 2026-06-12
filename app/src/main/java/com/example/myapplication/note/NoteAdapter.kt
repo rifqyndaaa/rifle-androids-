ea7f0afcbbd2fc94313b6e5f62c78184146169dc
lab151-35 <yanda24si@mahasiswa.pcr.ac.id>
2026-06-12 10:01:38 +0700

    p12 lab revisi

---
+++ app/src/main/java/com/example/myapplication/note/NoteAdapter.kt
@@ -0,0 +1,35 @@
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.databinding.ItemNoteBinding
+
+class NoteAdapter(
+    private var notes: List<NoteEntity>
+) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    inner class NoteViewHolder(val binding: ItemNoteBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val binding = ItemNoteBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return NoteViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.binding.tvTitle.text = note.title
+        holder.binding.tvContent.text = note.content
+    }
+
+    override fun getItemCount(): Int = notes.size
+
+    // 🔥 penting biar bisa update
+    fun updateData(newData: List<NoteEntity>) {
+        notes = newData
+        notifyDataSetChanged()
+    }
+}
